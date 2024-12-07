1ca0e44e839bc606b0206a0b687c8ee52bd76b9e
accarto <117378727+accarto@users.noreply.github.com>
2024-12-07 09:11:03 +0800

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -75,7 +75,7 @@
 #define MICRO 0.000001
 #define MILLI 0.001
 
-const std::string strDefaultFortuneAddress = "FkqwU8tUU6U41PuGTPUqy93hML4QtCDPDX";
+const std::string strDefaultFortuneAddress = "fYFrirwfyAbA2YEEYZN9dJXzidfvQ9C2ar";
 
 bool CBlockIndexWorkComparator::operator()(const CBlockIndex *pa, const CBlockIndex *pb) const {
     // First sort by most total work, ...
